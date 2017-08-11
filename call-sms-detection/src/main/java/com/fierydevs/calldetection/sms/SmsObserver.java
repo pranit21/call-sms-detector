99290a67aa805bef5311463a1cbf975b2083f3ab
Pranit More <morepranit@gmail.com>
2017-08-11 12:57:08 +0530

    updated version

--- call-sms-detection/src/main/java/com/fierydevs/calldetection/sms/SmsObserver.java
+++ call-sms-detection/src/main/java/com/fierydevs/calldetection/sms/SmsObserver.java
@@ -30,32 +30,34 @@ public class SmsObserver extends ContentObserver {
     @Override
     public void onChange(boolean selfChange) {
         super.onChange(selfChange);
-        // save the message to the SD card here
-        Uri uriSMSURI = Uri.parse("content://sms/sent");
-        Cursor cur = context.getContentResolver().query(uriSMSURI, null, null, null, null);
-        // this will make it point to the first record, which is the last SMS sent
-        if (cur != null) {
-            cur.moveToNext();
-            String id = cur.getString(cur.getColumnIndex("_id"));
-            if (smsChecker(id)) {
-                String address = cur.getString(cur.getColumnIndex("address"));
-                String content = cur.getString(cur.getColumnIndex("body"));
-                String date = cur.getString(cur.getColumnIndex("date"));
-                //String sim_id = cur.getString(cur.getColumnIndex("sim_id"));
-                /*Log.e("sms", content);
-                Log.e("smsNumber", address);
-                Log.e("date", date);*/
-                //Log.e("sim_id", sim_id);
-                String contactName = CallSmsDetector.retrieveContactName(context, address);
+        try {
+            // save the message to the SD card here
+            Uri uriSMSURI = Uri.parse("content://sms/sent");
+            Cursor cur = context.getContentResolver().query(uriSMSURI, null, null, null, null);
+            // this will make it point to the first record, which is the last SMS sent
+            if (cur != null) {
+                cur.moveToNext();
+                String id = cur.getString(cur.getColumnIndex("_id"));
+                if (smsChecker(id)) {
+                    String address = cur.getString(cur.getColumnIndex("address"));
+                    String content = cur.getString(cur.getColumnIndex("body"));
+                    String date = cur.getString(cur.getColumnIndex("date"));
+                    //String sim_id = cur.getString(cur.getColumnIndex("sim_id"));
+                    /*Log.e("sms", content);
+                    Log.e("smsNumber", address);
+                    Log.e("date", date);*/
+                    //Log.e("sim_id", sim_id);
+                    String contactName = CallSmsDetector.retrieveContactName(context, address);
 
-                mSentListener.onMessageSent(address, contactName, content, Long.parseLong(date));
+                    mSentListener.onMessageSent(address, contactName, content, Long.parseLong(date));
 
                 /*for (String column: cur.getColumnNames()) {
                     Log.e("cur.getColumnNames()", column);
                 }*/
-                cur.close();
+                    cur.close();
+                }
             }
-        }
+        } catch(Exception e) {}
     }
 
     private boolean smsChecker(String sms) {
